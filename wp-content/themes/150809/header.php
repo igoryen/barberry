105614aaee40836e05ad15455855fc6de8ad7b5d
igoryen <igor.yentaltsev@gmail.com>
2015-08-09 13:09:27 -0400

    Site title is now a link to home page

--- wp-content/themes/150809/header.php
+++ wp-content/themes/150809/header.php
@@ -10,7 +10,7 @@
   <body <?php body_class(); ?> >
 
     <header class="site-header">
-      <h1><?php bloginfo('name'); ?></h1>
+      <h1><a href="<?php echo home_url();?>"<?php bloginfo('name'); ?></h1>
       <h5><?php bloginfo('description'); ?></h5>
     </header>
 
